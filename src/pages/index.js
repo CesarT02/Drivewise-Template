80a1d3f80164a333b43be62b896e2b314533a2a8
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-02 23:12:21 -0700

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,37 @@
+import React from "react"
+import Layout  from "../components/Layout/Layout"
+
+export function Head() {
+    return (
+        <>
+            <title>Home | DriveWise</title>
+            <meta name="description" content="Justin Johnson is a web developer in Tucson, Arizona."/>
+            <link rel="canonical" href="Www.drivewise.site"/>
+        </>
+    )
+}
+
+export default function IndexPage() {
+    return (
+        <Layout>
+              <article>
+                <h2 class="px-2 text-xl md:text-2xl lg:text-3xl font-bold text-yellow-400">
+                    % echo "Welcome to Drivewise!"&nbsp;
+                    <span class="animate-pulse">&#x2588;</span>
+                </h2>
+                <div class="w-full mt-8 mb-8 mx-auto border border-b-0 border-pink-600"></div>
+                <p class="px-4 text-lg font-bold">Welcome to DriveWise, a predictive analytics project aimed at estimating the likelihood of car accidents in your area. Our website is designed to provide drivers with a deeper understanding of the level of risk associated with driving in a particular region, based on historical data of car accidents collected over a certain period.
+
+
+                                                   Using weather conditions, time of day and location, we analyze and interpret this data to determine higher risk roads or accident hotspots, which we present to you through an interactive map on our website. In addition, we provide raw data visualization lists of the numbers where users could filter through the data and see totals of incidents with different traits.
+
+
+                                                   Our ultimate goal is to help reduce the number of car accidents by providing suggestions and warnings if a certain condition like the time of day and or where weather conditions lines up to multiple accidents. This would be more informational with some usable functionality for the everyday user as well.
+
+
+                                                   With DriveWise, you can make more informed decisions when traveling and potentially avoid dangerous situations. Enter your location or a selected location to see the data near you and stay safe on the road.
+                </p>
+              </article>
+        </Layout>
+    )
+}
