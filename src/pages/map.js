f28a64e601dd66adceca3ff9323066aa1f8a2b07
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-03 14:02:11 -0700

    Update map.js

--- src/pages/map.js
+++ src/pages/map.js
@@ -14,28 +14,9 @@ export function Head() {
 export default function ProjectsPage() {
     return (
         <Layout>
-            <article class="space-y-2">
-                <h2 class="text-xl font-bold underline">Projects</h2>
-                <p>As I figure out how I want to implement projects on this page, let the site itself serve as one:</p>
-                <h3 class="font-bold underline">justinmjoh.com</h3>
-                <ul class="list-disc pl-8">
-                    <li class="text-cyan-600">
-                        <span class="text-slate-200">
-                            <a class="duration-200 hover:text-pink-600" href="https://tailwindcss.com/">Tailwind</a> (frontend / CSS)
-                        </span>
-                    </li>
-                    <li class="text-cyan-600">
-                        <span class="text-slate-200">
-                            <a class="duration-200 hover:text-pink-600" href="https://www.gatsbyjs.com/">GatsbyJS</a> (rendering)
-                        </span>
-                    </li>
-                    <li class="text-cyan-600">
-                        <span class="text-slate-200">
-                            <a class="duration-200 hover:text-pink-600" href="https://www.gatsbyjs.com/products/cloud/">Gatsby Cloud</a> (server)
-                        </span>
-                    </li>
-                </ul>
-            </article>
+            <div>    
+                <iframe style="width: 100%; height: 100%;" src="map-raw" title="description"></iframe>
+           </div>
         </Layout>
     )
 }
