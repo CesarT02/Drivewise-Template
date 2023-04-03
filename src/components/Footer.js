44575c5f9658a7306c66dcc7cc4e47d8ca48f45f
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-02 23:55:06 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import {FaGithub, FaInstagram, FaLinkedinIn, FaTwitter} from 'react-icons/fa'
+
 
 export default function Footer() {
     return (
         <footer className="bg-slate-900 py-2 border-t border-pink-600 text-center">
             <div className="inline-flex space-x-4 md:space-x-2 py-2 px-2 border border-yellow-400 rounded-xl">
                
+            </div>
             <p className="text-sm py-2 px-2 font-vcr">&copy; { new Date().getFullYear() } by Group 8</p>
         </footer>
     )
