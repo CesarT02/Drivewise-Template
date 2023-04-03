431091edbbf5b2ccb4de9fd68b61de05297490e2
CesarT02 <127069401+CesarT02@users.noreply.github.com>
2023-04-02 21:21:32 -1000

    4/2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
-import photo from '../images/about-photo.jpeg'
+import photo from '../images/about-photo.png'
 
 export function Head() {
     return (
