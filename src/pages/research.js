61ed05d3ed6e8e1d6b7027461ac1cdbfda738a9e
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-03 22:58:05 -0700

    Update research.js

--- src/pages/research.js
+++ src/pages/research.js
@@ -15,23 +15,23 @@ export default function ResearchPage() {
     return (
         <Layout>
             <article class="space-y-2">
-                <h2 class="text-xl font-bold underline">Projects</h2>
-                <p>As I figure out how I want to implement projects on this page, let the site itself serve as one:</p>
-                <h3 class="font-bold underline">justinmjoh.com</h3>
+                <h2</h2>
+                <p>:</p>
+                <h3 </h3>
                 <ul class="list-disc pl-8">
                     <li class="text-cyan-600">
                         <span class="text-slate-200">
-                            <a class="duration-200 hover:text-pink-600" href="https://tailwindcss.com/">Tailwind</a> (frontend / CSS)
+                            <a 
                         </span>
                     </li>
                     <li class="text-cyan-600">
                         <span class="text-slate-200">
-                            <a class="duration-200 hover:text-pink-600" href="https://www.gatsbyjs.com/">GatsbyJS</a> (rendering)
+                            <a 
                         </span>
                     </li>
                     <li class="text-cyan-600">
                         <span class="text-slate-200">
-                            <a class="duration-200 hover:text-pink-600" href="https://www.gatsbyjs.com/products/cloud/">Gatsby Cloud</a> (server)
+                            <a 
                         </span>
                     </li>
                 </ul>
