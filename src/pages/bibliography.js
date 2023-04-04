9f9e88d9f52173c55b12efea84364be1f34979c3
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-04 00:26:37 -0700

    Update bibliography.js

--- src/pages/bibliography.js
+++ src/pages/bibliography.js
@@ -13,29 +13,11 @@ export function Head() {
 
 export default function BibliographyPage() {
     return (
-        <Layout>
+     <Layout>
             <article class="space-y-2">
-                <h2 class="text-xl font-bold underline">Projects</h2>
-                <p>As I figure out how I want to implement projects on this page, let the site itself serve as one:</p>
-                <h3 class="font-bold underline">justinmjoh.com</h3>
-                <ul class="list-disc pl-8">
-                    <li class="text-cyan-600">
-                        <span class="text-slate-200">
-                            <a class="duration-200 hover:text-pink-600" href="https://tailwindcss.com/">Tailwind</a> (frontend / CSS)
-                        </span>
-                    </li>
-                    <li class="text-cyan-600">
-                        <span class="text-slate-200">
-                            <a class="duration-200 hover:text-pink-600" href="https://www.gatsbyjs.com/">GatsbyJS</a> (rendering)
-                        </span>
-                    </li>
-                    <li class="text-cyan-600">
-                        <span class="text-slate-200">
-                            <a class="duration-200 hover:text-pink-600" href="https://www.gatsbyjs.com/products/cloud/">Gatsby Cloud</a> (server)
-                        </span>
-                    </li>
-                </ul>
+                <h2 class="text-xl font-bold underline">Bibliography</h2>
+                <p>Bibliography goes here:</p>
             </article>
-        </Layout>
+    </Layout>
     )
 }
