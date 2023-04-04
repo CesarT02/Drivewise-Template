386ad19fe98e61219448b96b6033ae964e136114
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-03 23:03:21 -0700

    Update bibliography.js

--- src/pages/bibliography.js
+++ src/pages/bibliography.js
@@ -6,7 +6,7 @@ export function Head() {
         <>
             <title>Bibliography | Group 8</title>
             <meta name="description" content="Group 8"/>
-            <link rel="canonical" href="https://www.drivewise.site/Bibliography"/>
+            <link rel="canonical" href="www.drivewise.site/bibliography"/>
         </>
     )
 }
