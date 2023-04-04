b62004811cc8bf7a7a93d514dd2eedd5e91ccdd2
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-03 23:01:12 -0700

    Update research.js

--- src/pages/research.js
+++ src/pages/research.js
@@ -6,7 +6,7 @@ export function Head() {
         <>
             <title>Projects | Group 8</title>
             <meta name="description" content="Group 8"/>
-            <link rel="canonical" href="https://www.drivewise.site/Research"/>
+            <link rel="canonical" href="www.drivewise.site/research"/>
         </>
     )
 }
