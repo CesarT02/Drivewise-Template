aac94b27275a59a06ae9c58e174d20a367e4cdec
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-06 20:52:28 -0700

    Update map.js

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
-import GoogleMap from './GoogleMap';
 
 export function Head() {
   // ...
 }
 
 export default function MapPage() {
-  return (
-    <Layout>
-      <GoogleMap />
-    </Layout>
-  );
+    return (
+        <Layout>
+            
+        </Layout>
+    )
 }
