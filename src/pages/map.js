50b8d4135666cc6dd452063278ae5283d3e1fc65
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-08 13:24:01 -0700

    Update map.js

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,5 +1,6 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import Layout from '../components/Layout/Layout';
+import { Loader } from '@googlemaps/js-api-loader';
 
 const mapContainerStyle = {
   width: '100%',
@@ -12,41 +13,61 @@ export function Head() {
 
 export default function MapPage() {
   const mapRef = useRef();
-  const [map, setMap] = useState(null);
-  const [heatmap, setHeatmap] = useState(null);
+  let heatmap;
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const loader = new window.google.maps.plugins.loader.Loader({
-        apiKey: 'AIzaSyAuOhlWr5cxsZcvX6FSWA_mcEfGAGqE6u8', // Replace with your API key
-        version: 'weekly',
-        libraries: ['visualization'],
+    const loader = new Loader({
+      apiKey: 'AIzaSyAuOhlWr5cxsZcvX6FSWA_mcEfGAGqE6u8', // Replace with your API key
+      version: 'weekly',
+      libraries: ['visualization'],
+    });
+
+    loader.load().then(() => {
+      const map = new google.maps.Map(mapRef.current, {
+        zoom: 13,
+        center: { lat: 37.775, lng: -122.434 },
+        mapTypeId: 'satellite',
       });
 
-      loader.load().then(() => {
-        const newMap = new window.google.maps.Map(mapRef.current, {
-          zoom: 13,
-          center: { lat: 37.775, lng: -122.434 },
-          mapTypeId: 'satellite',
-        });
-
-        setMap(newMap);
-
-        const newHeatmap = new window.google.maps.visualization.HeatmapLayer({
-          data: getPoints(),
-          map: newMap,
-        });
-
-        setHeatmap(newHeatmap);
+      heatmap = new google.maps.visualization.HeatmapLayer({
+        data: getPoints(),
+        map: map,
       });
-    }
+    });
   }, []);
 
   function toggleHeatmap() {
     heatmap.setMap(heatmap.getMap() ? null : map);
   }
 
-  // Other functions (changeGradient, changeRadius, changeOpacity) remain the same.
+  function changeGradient() {
+    const gradient = [
+      "rgba(0, 255, 255, 0)",
+      "rgba(0, 255, 255, 1)",
+      "rgba(0, 191, 255, 1)",
+      "rgba(0, 127, 255, 1)",
+      "rgba(0, 63, 255, 1)",
+      "rgba(0, 0, 255, 1)",
+      "rgba(0, 0, 223, 1)",
+      "rgba(0, 0, 191, 1)",
+      "rgba(0, 0, 159, 1)",
+      "rgba(0, 0, 127, 1)",
+      "rgba(63, 0, 91, 1)",
+      "rgba(127, 0, 63, 1)",
+      "rgba(191, 0, 31, 1)",
+      "rgba(255, 0, 0, 1)",
+    ];
+
+    heatmap.set("gradient", heatmap.get("gradient") ? null : gradient);
+  }
+
+  function changeRadius() {
+    heatmap.set("radius", heatmap.get("radius") ? null : 20);
+  }
+
+  function changeOpacity() {
+    heatmap.set("opacity", heatmap.get("opacity") ? null : 0.2);
+  }
 
   function getPoints() {
     return [
@@ -73,4 +94,3 @@ export default function MapPage() {
     </Layout>
   );
 }
-
