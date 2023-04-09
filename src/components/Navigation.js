2823ef3f868e6fc46e6f5565a04be2a8db60f513
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-08 18:21:54 -0700

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -13,6 +13,7 @@ export default function Navigation() {
                     <StyledNavLink to="/charts/">Charts</StyledNavLink>
                     <StyledNavLink to="/research/">Research</StyledNavLink>
                     <StyledNavLink to="/bibliography/">Bibliography</StyledNavLink>
+                    <StyledNavLink to="/CSV/">CSV</StyledNavLink>
                 </div>
             </div>
         </nav>
