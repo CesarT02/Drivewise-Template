409ad6ff41cc7f062bb7628b4349deab438b605b
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-09 19:32:58 -0700

    Update charts.js

--- src/pages/charts.js
+++ src/pages/charts.js
@@ -19,15 +19,10 @@ export default function ChartsPage() {
         <p>Charts go here:</p>
         <div className="flex flex-wrap justify-center">
           <div className="w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 px-4 mb-4">
-            <img src="path/to/first/image" alt="First Image" className="w-full" />
-            <img src="path/to/first/image" alt="First Image" className="w-full" />
-            <img src="path/to/first/image" alt="First Image" className="w-full" />
+            <img src="../images/chart1.png" alt="First Image" className="w-full" />
+            <img src="../images/chart-2.png" alt="Second Image" className="w-full" />
+            <img src="../images/chart-3.png" alt="Third Image" className="w-full" />
             <p className="text-center mt-2">Text under first image</p>
-          </div>
-          <div className="w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 px-4 mb-4">
-            <img src="path/to/second/image" alt="Second Image" className="w-full" />
-            <p className="text-center mt-2">Text under second image</p>
-          </div>
         </div>
       </article>
     </Layout>
