5776afdce9fe69bb8b4d9b513b5fd77baf3b1d84
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-11 23:00:28 -0700

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,7 +8,7 @@ export default function Navigation() {
             <div class="bg-slate-900 py-2 border-b border-pink-600 shadow-lg">
                 <div class="max-w-7xl md:mx-auto md:w-3/4 md:space-x-2 space-y-2 md:space-y-0">
                     <Logo/>
-                    <StyledNavLink to="/about/">About</StyledNavLink>
+                    
                     <StyledNavLink to="/map/">Map</StyledNavLink>
                     <StyledNavLink to="/charts/">Charts</StyledNavLink>
                     <StyledNavLink to="/research/">Research</StyledNavLink>
