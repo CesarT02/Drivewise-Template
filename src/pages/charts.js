f13349044d3fa323b37b21d6e54c6290f68e25a6
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-13 10:32:25 -0700

    Update charts.js

--- src/pages/charts.js
+++ src/pages/charts.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import Layout from '../components/Layout/Layout';
-import chart1 from '../images/chart1.png';
-import chart2 from '../images/chart-2.png';
-import chart3 from '../images/chart-3.png';
+import React from "react";
+import Layout from "../components/Layout/Layout";
+import chart1 from "../images/chart1.png";
+import chart2 from "../images/chart-2.png";
+import chart3 from "../images/chart-3.png";
 
 export function Head() {
   return (
@@ -20,30 +20,50 @@ export default function ChartsPage() {
       <article className="space-y-2">
         <h2 className="text-xl font-bold underline">Charts</h2>
         <p>Charts go here:</p>
-        <style jsx>{`
-          .image-container {
-            position: relative;
-          }
+        <style jsx>
+          <style jsx>{`
+  .image-container {
+    position: relative;
+  }
 
-          .image-container:hover .hovered-image {
-            display: block;
-          }
+  .image-container:hover .hovered-image {
+    display: block;
+  }
 
-          .hovered-image {
-            display: none;
-            position: absolute;
-            z-index: 10;
-            max-width: calc(100vw - 2rem);
-            max-height: calc(100vh - 2rem);
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-          }
-        `}</style>
+  .hovered-image {
+    display: none;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 10;
+    max-width: calc(100vw - 2rem);
+    max-height: calc(100vh - 2rem);
+    object-fit: contain;
+    background-color: rgba(255, 255, 255, 0.95);
+    padding: 1rem;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    border-radius: 0.5rem;
+  }
+`}</style>
+        </style>
         <div className="flex flex-wrap justify-center">
           {[chart1, chart2, chart3].map((image, index) => (
-            <div className="w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 px-4 mb-4" key={index}>
+            <div
+              className="w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 px-4 mb-4"
+              key={index}
+            >
               <div className="image-container">
-                <img src={image} alt={`Image ${index + 1}`} className="w-full cursor-pointer" />
-                <img src={image} alt={`Image ${index + 1}`} className="hovered-image" />
+                <img
+                  src={image}
+                  alt={`Image ${index + 1}`}
+                  className="w-full cursor-pointer"
+                />
+                <img
+                  src={image}
+                  alt={`Image ${index + 1}`}
+                  className="hovered-image"
+                />
               </div>
               <p className="text-center mt-2">Text under image {index + 1}</p>
             </div>
@@ -53,4 +73,3 @@ export default function ChartsPage() {
     </Layout>
   );
 }
-
