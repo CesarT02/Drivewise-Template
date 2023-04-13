8afea70fe7e5f9209c309cd0c8b0aad52d56f831
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-13 10:01:37 -0700

    Update charts.js

--- src/pages/charts.js
+++ src/pages/charts.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/Layout/Layout';
 import chart1 from '../images/chart1.png';
 import chart2 from '../images/chart-2.png';
@@ -15,38 +15,40 @@ export function Head() {
 }
 
 export default function ChartsPage() {
+  const [hoveredImage, setHoveredImage] = useState(null);
+
+  const handleMouseEnter = (index) => {
+    setHoveredImage(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredImage(null);
+  };
+
   return (
     <Layout>
       <article className="space-y-2">
         <h2 className="text-xl font-bold underline">Charts</h2>
         <p>Charts go here:</p>
-        <style jsx>{`
-          .chart-container {
-            position: relative;
-            overflow: hidden;
-            padding-bottom: 100%;
-          }
-
-          .chart-container img {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            transition: transform 0.3s ease-in-out;
-          }
-
-          .chart-container:hover img {
-            transform: scale(3.5);
-            z-index: 1;
-          }
-        `}</style>
         <div className="flex flex-wrap justify-center">
           {[chart1, chart2, chart3].map((image, index) => (
-            <div className="w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 px-4 mb-4" key={index}>
-              <div className="chart-container">
-                <img src={image} alt={`Image ${index + 1}`} />
+            <div
+              className={`w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 px-4 mb-4 ${
+                hoveredImage === index ? 'w-full h-full fixed top-0 left-0 z-50' : ''
+              }`}
+              key={index}
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={handleMouseLeave}
+            >
+              <div className="relative overflow-hidden" style={{ paddingBottom: '100%' }}>
+                <img
+                  src={image}
+                  alt={`Image ${index + 1}`}
+                  className="absolute inset-0 w-full h-full object-cover transition-transform duration-300 ease-in-out"
+                  style={{
+                    transform: hoveredImage === index ? 'scale(1)' : 'scale(1)',
+                  }}
+                />
               </div>
               <p className="text-center mt-2">Text under image {index + 1}</p>
             </div>
