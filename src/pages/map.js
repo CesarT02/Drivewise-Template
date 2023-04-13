0a50aaa0855427c03d191c8e7e1a364962604662
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-13 10:08:18 -0700

    Update map.js

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,209 +1,69 @@
-import React, { useRef, useEffect, useState } from "react";
-import Layout from "../components/Layout/Layout";
-import { Loader } from "@googlemaps/js-api-loader";
-import * as XLSX from 'xlsx';
+import React, { useState } from 'react';
+import Layout from '../components/Layout/Layout';
+import chart1 from '../images/chart1.png';
+import chart2 from '../images/chart-2.png';
+import chart3 from '../images/chart-3.png';
 
-const mapContainerStyle = {
-  width: "100%",
-  height: "100vh",
-};
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-const apiKey = "AIzaSyAuOhlWr5cxsZcvX6FSWA_mcEfGAGqE6u8";
-
-async function getLatLngFromStreetName(streetName, city, state, apiKey) {
-  const response = await fetch(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-      `${streetName}, ${city}, ${state}`
-    )}&key=${apiKey}`
+export function Head() {
+  return (
+    <>
+      <title>Charts | Group 8</title>
+      <meta name="description" content="Group 8" />
+      <link rel="canonical" href="www.drivewise.site/charts" />
+    </>
   );
-  const data = await response.json();
-  if (data.status === "OK" && data.results.length > 0) {
-    const location = data.results[0].geometry.location;
-    console.log("Location:", location);
-    return new google.maps.LatLng(location.lat, location.lng);
-  } else {
-    throw new Error("Failed to geocode street name");
-  }
 }
 
-async function parseAndGeocodeExcel(excelArrayBuffer, apiKey, filterFunction) {
-  const workbook = XLSX.read(new Uint8Array(excelArrayBuffer), { type: 'array' });
-  const firstSheetName = workbook.SheetNames[0];
-  const worksheet = workbook.Sheets[firstSheetName];
-  const json = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+export default function ChartsPage() {
+  const [hoveredImage, setHoveredImage] = useState(null);
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
 
-  const headers = json.shift();
-  const data = json.map(row => headers.reduce((acc, header, i) => {
-    acc[header] = row[i];
-    return acc;
-  }, {}));
+  const handleMouseEnter = (index) => {
+    setHoveredImage(index);
+  };
 
-  const nonEmptyRows = data.filter((row) => row.StreetName.trim() !== '');
-  const filteredRows = nonEmptyRows.filter(filterFunction);
-  const coordinates = [];
+  const handleMouseLeave = () => {
+    setHoveredImage(null);
+  };
 
-    for (const row of filteredRows) {
-    try {
-      const latLng = await getLatLngFromStreetName(row.StreetName, row.City, row.State, apiKey);
-      coordinates.push(latLng);
-      console.log("LatLng:", latLng);
-    } catch (error) {
-      console.error(`Failed to geocode street name "${row.StreetName}":`, error);
-    }
-    await sleep(200); // Adjust the sleep time as needed (in milliseconds)
-  }
-  console.log(coordinates);
-
-  return coordinates;
-}
-
-function createCustomGradient(colors) {
-  const gradient = colors
-    .map((color) => `rgba(${color.join(",")}, 0)`)
-    .concat(colors.map((color) => `rgba(${color.join(",")}, 1)`));
-  return gradient;
-}
-
-export default function MapPage() {
-  const mapRef = useRef();
-  const [map, setMap] = useState(null);
-  const [heatmap, setHeatmap] = useState(null);
-
-  useEffect(() => {
-    const loader = new Loader({
-      apiKey: apiKey,
-      version: "weekly",
-      libraries: ["visualization"],
-    });
-
-    loader.load().then(() => {
-      const loadedMap = new google.maps.Map(mapRef.current, {
-        zoom: 13,
-        center: { lat: 32.248814, lng: -110.987419 },
-        mapTypeId: "satellite",
-      });
-
-      setMap(loadedMap);
-    });
-  }, []);
-
-async function loadHeatmapData(filterFunction, gradientColors) {
-  const excelResponse = await fetch("/Good_Excel.xlsx");
-  const excelBlob = await excelResponse.blob();
-
-  const excelArrayBuffer = await new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onloadend = () => resolve(reader.result);
-    reader.onerror = reject;
-    reader.readAsArrayBuffer(excelBlob);
-  });
-
-  const coordinates = await parseAndGeocodeExcel(
-    excelArrayBuffer,
-    apiKey,
-    filterFunction
-  );
-
-  if (heatmap) {
-    heatmap.setMap(null);
-  }
-
-  const newHeatmap = new google.maps.visualization.HeatmapLayer({
-   data: coordinates,
-   map,
-   radius: 20,
-   opacity: 0.7,
-   gradient: gradientColors ? createCustomGradient(gradientColors) : null,
- });
-  console.log(coordinates);
-
-  setHeatmap(newHeatmap);
-}
-
-function filterByVehicleCollision(data) {
-  const allowedTypes = ["Vehicle/Vehicle"];
-  const vehicleCollisionData = data.Collision || "";
-
-  const result = allowedTypes.includes(vehicleCollisionData.trim());
-  console.log("VehicleCollision filter:", data, result);
-  return result;
-}
-
-function filterByWeatherAndDay(data) {
-  const allowedWeather = ["CLEAR", "CLOUDY", "RAIN", "SLEET / HA"];
-  const allowedDay = [
-  "DAYLIGHT",
-  "DARK",
-  "DUSK",
-  "DAWN",
-  "DARK-LIGHTED",
-  "DARK-NOT LIGHTED",
-];
-  const weather = data.Weather ? data.Weather.trim() : '';
-  const day = data.Day ? data.Day.trim() : '';
-  const result =
-    allowedWeather.includes(weather) &&
-    allowedDay.includes(day);
-  console.log("WeatherAndDay filter:", data, result);
-  return result;
-}
- function switchToVehicleCollisionData() {
-  loadHeatmapData(filterByVehicleCollision, [
-    [0, 255, 255],
-    [0, 191, 255],
-    [0, 127, 255],
-    [0, 63, 255],
-    [0, 0, 255],
-    [0, 0, 223],
-    [0, 0, 191],
-    [0, 0, 159],
-    [0, 0, 127],
-   ]).then(coordinates => {
-    console.log('VehicleCollision coordinates:', coordinates);
-  });
-}
-
-  function switchToWeatherAndDayData() {
-    loadHeatmapData(filterByWeatherAndDay, [
-      [255, 255, 0],
-      [255, 191, 0],
-      [255, 127, 0],
-      [255, 63, 0],
-      [255, 0, 0],
-      [223, 0, 0],
-      [191, 0, 0],
-      [159, 0, 0],
-      [127, 0, 0],
-    ]).then(coordinates => {
-    console.log('WeatherAndDay coordinates:', coordinates);
-  });
-}
-  function switchToOriginalData() {
-    loadHeatmapData(() => true); // Pass a function that always returns true to include all data points
-  }
+  const handleMouseMove = (event) => {
+    setCursorPosition({ x: event.clientX, y: event.clientY });
+  };
 
   return (
     <Layout>
-      <button
-        id="switch-to-vehicle-collision-data"
-        onClick={switchToVehicleCollisionData}
-      >
-        Switch to Vehicle Collision Data
-      </button>
-      <button
-        id="switch-to-weather-and-day-data"
-        onClick={switchToWeatherAndDayData}
-      >
-        Switch to Weather and Day Data
-      </button>
-      <button id="switch-to-original-data" onClick={switchToOriginalData}>
-        Switch to Original Data
-      </button>
-      <div ref={mapRef} style={mapContainerStyle} />
+      <article className="space-y-2">
+        <h2 className="text-xl font-bold underline">Charts</h2>
+        <p>Charts go here:</p>
+        <div className="flex flex-wrap justify-center">
+          {[chart1, chart2, chart3].map((image, index) => (
+            <div className="w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 px-4 mb-4" key={index}>
+              <img
+                src={image}
+                alt={`Image ${index + 1}`}
+                className="w-full cursor-pointer"
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={handleMouseLeave}
+                onMouseMove={handleMouseMove}
+              />
+              <p className="text-center mt-2">Text under image {index + 1}</p>
+            </div>
+          ))}
+        </div>
+        {hoveredImage !== null && (
+          <div
+            className="fixed z-50 p-2 bg-white border border-gray-200 shadow-lg"
+            style={{
+              top: cursorPosition.y + 15,
+              left: cursorPosition.x + 15,
+              transform: 'translateX(-50%)',
+              maxWidth: 'calc(100% - 30px)',
+            }}
+          >
+            <img src={[chart1, chart2, chart3][hoveredImage]} alt={`Image ${hoveredImage + 1}`} className="w-full" />
+          </div>
+        )}
+      </article>
     </Layout>
   );
 }
