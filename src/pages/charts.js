ebe6e5f7a3c7b551ad9dfdc08e09e79b4af38c1d
Dannyg124 <127724123+Dannyg124@users.noreply.github.com>
2023-04-13 19:27:18 -0700

    Update charts.js

--- src/pages/charts.js
+++ src/pages/charts.js
@@ -26,37 +26,36 @@ export default function ChartsPage() {
       <article className="space-y-2">
         <h2 className="text-xl font-bold underline">Charts</h2>
         
-        <style jsx>
-          <style jsx>{`
-  .image-container {
-    position: relative;
-  }
-  .image-container:hover .hovered-image {
-    display: block;
-  }
-  .hovered-image {
-    display: none;
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 10;
-    max-width: calc(80vw - 2rem);
-    max-height: calc(80vh - 2rem);
-    object-fit: contain;
-    background-color: rgba(255, 255, 255, 0.95);
-    padding: 1rem;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    border-radius: 0.5rem;
-  }
-`}</style>
-        </style>
+        <style jsx>{`
+          .image-container {
+            position: relative;
+          }
+          .image-container:hover .hovered-image {
+            display: block;
+          }
+          .hovered-image {
+            display: none;
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            z-index: 10;
+            max-width: calc(80vw - 2rem);
+            max-height: calc(80vh - 2rem);
+            object-fit: contain;
+            background-color: rgba(255, 255, 255, 0.95);
+            padding: 1rem;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            border-radius: 0.5rem;
+          }
+        `}</style>
+        
         <div className="flex flex-wrap justify-center">
           {[picture1, picture2, chart3].map((image, index) => (
             <div
               className="w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 px-4 mb-4"
               key={index}
-            >
+                >
               <div className="image-container">
                 <img
                   src={image}
